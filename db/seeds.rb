160d23af0b68a0dc37e88146636fc398550c2763
David Whitlatch <davidtwhitlatch@gmail.com>
2021-06-21 11:16:06 -0400

    BE added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Flavor.destroy_all
+Food.destroy_all
+User.destroy_all
+
+@user = User.create!(username: 'Loon', email: 'loon@email.com', password: '123456')
+
+puts "#{User.count} users created"
+
+@salty = Flavor.create!(name: 'Salty')
+@sweet = Flavor.create!(name: 'Sweet')
+@sour = Flavor.create!(name: 'Sour')
+@bitter = Flavor.create!(name: 'Bitter')
+@umami = Flavor.create!(name: 'Umami')
+
+puts "#{Flavor.count} flavors created"
+
+@pizza = Food.create!(name: 'Pizza', user: @user)
+@ice_cream = Food.create!(name: 'Ice Cream', user: @user)
+@sushi = Food.create!(name: 'Sushi', user: @user, flavors: [@salty, @umami])
+@tacos = Food.create!(name: 'Tacos', user: @user)
+
+puts "#{Food.count} foods created"
+
+@pizza.flavors.push(@salty, @sweet, @umami)
+@ice_cream.flavors = [@sweet, @salty]
+@tacos.flavors << @salty
+@tacos.flavors << @umami
+@tacos.flavors << @sour
+@tacos.flavors << @sweet
+
+
