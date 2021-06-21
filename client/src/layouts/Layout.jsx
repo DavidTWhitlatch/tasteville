2b79098c0254250c1ab86a5da272666355ea0d37
David Whitlatch <davidtwhitlatch@gmail.com>
2021-06-21 14:41:08 -0400

    FE auth setup

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,13 +1,29 @@
 import { Link } from 'react-router-dom';
 
 export default function Layout(props) {
+	const { currentUser, handleLogout, children } = props;
 	return (
 		<div>
 			<header>
 				<h1>Tasteville</h1>
-        <Link to='/login'>Login/Register</Link>
+				{currentUser ? (
+					<>
+						<p>{currentUser.username}</p>
+						<button onClick={handleLogout}>Logout</button>
+					</>
+				) : (
+					<Link to='/login'>Login/Register</Link>
+				)}
+				<hr />
+        {
+          currentUser && 
+          <>
+            <Link to='/foods'>Foods</Link>
+            <Link to='/flavors'>Flavors</Link>
+          </>
+        }
 			</header>
-			{props.children}
+			{children}
 		</div>
 	);
 }
